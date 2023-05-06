9d919982b0fc75becd4aaf793b7aff7397da7762
MiguelMurillo07 <jose2224644@correo.uis.edu.co>
2023-05-06 11:23:05 -0500

    Inicio ComboBox

--- modelo/Plan.java
+++ modelo/Plan.java
@@ -16,8 +16,50 @@ public class Plan
         this.valorMinuto = pValorMinuto;
     }
 
+    public Plan()
+    {
+        
+    }
+
     public int getNumeroCelular()
     {
-        return 
+        return this.numeroCelular;
     }
+
+    public String getOperador()
+    {
+        return this.operador;
+    }
+
+    public int getCantidadMinutos()
+    {
+        return this.cantidadMinutos;
+    }
+
+    public double getValorMinuto()
+    {
+        return this.valorMinuto;
+    }  
+
+    public double calcularValorPlan()
+    {
+        return this.cantidadMinutos * this.valorMinuto;
+    }
+
+    public double calcularValorPlan(int pCantidadMinutos)
+    {
+        return pCantidadMinutos * this.valorMinuto;
+    }
+
+    public double calcularValorPlan(double pValorMinuto)
+    {
+        return this.cantidadMinutos * pValorMinuto;
+    }
+
+    public double calcularValorPlan(int pCantidadMinutos, double pValorMinuto)
+    {
+        return pCantidadMinutos * pValorMinuto;
+    }
+
+
 }
